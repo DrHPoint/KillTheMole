3e8001e2c9bc1fae1f74091d2e76f9bc3df92bd8
Pavel <hpedlk1007@gmail.com>
2019-10-04 15:40:43 +0800

    update some moments
    Выполнил почти все, что намечал ранее:
    
    1) Запретил на все активи делать ориентацию лэндскейп.
    
    2) Убрал почти все топорные моменты. Один из моментов не получилось убрать. Это касается того участка кода, где прописываются картинки на бэкграунд всех кнопок. Пытался все id кнопок перевесить на переменную, и уже от нее менять бэк, но получил ошибку: 
    
    findViewById не может быть нулевым
    Не знаю как именно тут привести к MVC, так как тут не могу вывести модель, которую могу вывести в отдельный класс. Все функции связаны либо с onclick, либо связанны с countdown.
    
    3) заменил кружки на спрайты крота из чешского мультика.
    
    4) Также по окончанию в строке состояния, после "Game Over" появляется счет, а после этого действие переносится на главный экран. Попытался использовать Intent для более плавного перехода, но столкнулся с какой-то непонятной ошибкой, в связи с этим использовал 
    
    exitProcess(-1)
    В целом, если изменить пару деталей, можно добавить больше кнопок, но перед этим нужно решить проблему описанную в пункте 2)

--- KillTheMole/app/src/main/java/ru/teslateam/killthemole/KillTheMoleActivity.kt
+++ KillTheMole/app/src/main/java/ru/teslateam/killthemole/KillTheMoleActivity.kt
@@ -1,5 +1,6 @@
 package ru.teslateam.killthemole
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -9,7 +10,9 @@ import kotlinx.android.synthetic.main.activity_kill_the_mole.*
 import kotlin.math.ceil
 import kotlin.math.floor
 import kotlin.math.round
+import android.os.Handler
 import kotlin.random.Random
+import kotlin.system.exitProcess
 
 
 class KillTheMoleActivity : AppCompatActivity() {
@@ -18,24 +21,18 @@ class KillTheMoleActivity : AppCompatActivity() {
     var moleScoreNumber = 0
     var score = 0
 
+    fun ScorePlus() {
+        score++
+        moleScoreNumber = 0
+    }
+
     fun onClickMole(view: View) {
-        when((view as Button).id) {
-            firstMole.id -> if (moleScoreNumber == 1) {
-                score++
-                moleScoreNumber = 0
-            }
-            secondMole.id -> if (moleScoreNumber == 2) {
-                score++
-                moleScoreNumber = 0
-            }
-            thirdMole.id -> if (moleScoreNumber == 3) {
-                score++
-                moleScoreNumber = 0
-            }
-            forthMole.id -> if (moleScoreNumber == 4) {
-                score++
-                moleScoreNumber = 0
-            }
+        view.setBackgroundResource(R.drawable.empty)
+        when(view.id) {
+            firstMole.id -> if (moleScoreNumber == 1) ScorePlus()
+            secondMole.id -> if (moleScoreNumber == 2) ScorePlus()
+            thirdMole.id -> if (moleScoreNumber == 3) ScorePlus()
+            forthMole.id -> if (moleScoreNumber == 4) ScorePlus()
         }
         ScoreOfKill.text = score.toString()
     }
@@ -52,29 +49,29 @@ class KillTheMoleActivity : AppCompatActivity() {
         fun moleanimation(a: Int) {
             when(a) {
                 3 -> when(moleNumber) {
-                    1 -> firstMole.setBackgroundResource(R.drawable.red)
-                    2 -> secondMole.setBackgroundResource(R.drawable.red)
-                    3 -> thirdMole.setBackgroundResource(R.drawable.red)
-                    4 -> forthMole.setBackgroundResource(R.drawable.red)
+                    1 -> firstMole.setBackgroundResource(R.drawable.mole1)
+                    2 -> secondMole.setBackgroundResource(R.drawable.mole1)
+                    3 -> thirdMole.setBackgroundResource(R.drawable.mole1)
+                    4 -> forthMole.setBackgroundResource(R.drawable.mole1)
                 }
                 2 -> when(moleNumber) {
-                    1 -> firstMole.setBackgroundResource(R.drawable.yellow)
-                    2 -> secondMole.setBackgroundResource(R.drawable.yellow)
-                    3 -> thirdMole.setBackgroundResource(R.drawable.yellow)
-                    4 -> forthMole.setBackgroundResource(R.drawable.yellow)
+                    1 -> firstMole.setBackgroundResource(R.drawable.mole2)
+                    2 -> secondMole.setBackgroundResource(R.drawable.mole2)
+                    3 -> thirdMole.setBackgroundResource(R.drawable.mole2)
+                    4 -> forthMole.setBackgroundResource(R.drawable.mole2)
                 }
                 1 -> when(moleNumber) {
-                    1 -> firstMole.setBackgroundResource(R.drawable.green)
-                    2 -> secondMole.setBackgroundResource(R.drawable.green)
-                    3 -> thirdMole.setBackgroundResource(R.drawable.green)
-                    4 -> forthMole.setBackgroundResource(R.drawable.green)
+                    1 -> firstMole.setBackgroundResource(R.drawable.mole3)
+                    2 -> secondMole.setBackgroundResource(R.drawable.mole3)
+                    3 -> thirdMole.setBackgroundResource(R.drawable.mole3)
+                    4 -> forthMole.setBackgroundResource(R.drawable.mole3)
                 }
                 0 -> {
                     when (moleNumber) {
-                        1 -> firstMole.setBackgroundResource(R.drawable.black)
-                        2 -> secondMole.setBackgroundResource(R.drawable.black)
-                        3 -> thirdMole.setBackgroundResource(R.drawable.black)
-                        4 -> forthMole.setBackgroundResource(R.drawable.black)
+                        1 -> firstMole.setBackgroundResource(R.drawable.empty)
+                        2 -> secondMole.setBackgroundResource(R.drawable.empty)
+                        3 -> thirdMole.setBackgroundResource(R.drawable.empty)
+                        4 -> forthMole.setBackgroundResource(R.drawable.empty)
                     }
                     moleNumber = 0
                     moleScoreNumber = 0
@@ -83,9 +80,11 @@ class KillTheMoleActivity : AppCompatActivity() {
         }
 
         val mole = object: CountDownTimer(1600, 500) {
-
             override fun onTick(millisUntilFinished: Long) {
-                moleanimation(floor(millisUntilFinished.toDouble()/500).toInt())
+                if(moleNumber ==moleScoreNumber)
+                    moleanimation(floor(millisUntilFinished.toDouble()/500).toInt())
+                else
+                    moleanimation(0)
             }
             override fun onFinish() {
                 moleanimation(0)
@@ -94,15 +93,20 @@ class KillTheMoleActivity : AppCompatActivity() {
         val game = object: CountDownTimer(60100, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 TimeToKill.text = floor(millisUntilFinished.toDouble()/1000).toInt().toString()
-                if (floor(millisUntilFinished.toDouble()/1000).toInt()%5 == 0) {
+                if (floor(millisUntilFinished.toDouble()/1000).toInt()%3 == 0) {
                     RandMole()
                     mole.start()
                 }
-
             }
             override fun onFinish() {
                 TimeToKill.text = "0"
                 textToReady.text = "Game Over"
+                Handler().postDelayed({
+                    textToReady.text = "Result: $score"
+                    Handler().postDelayed({
+                        exitProcess(-1)
+                    }, 1000)
+                }, 500)
             }
         }
         val timer = object: CountDownTimer(3100, 1000) {
